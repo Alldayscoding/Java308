63ef0d1f7deb17ef573d137fb3422a6cfe252ff8
Jason <jason.kim3221@gmail.com>
2023-04-05 18:01:07 +0900

    2023-04-05

--- Java_20230403/src/test/VarEx1.java
+++ Java_20230403/src/test/VarEx1.java
@@ -1,11 +1,14 @@
 package test;
-
 public class VarEx1 {
-
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		
+		boolean t = true;
+		
+		boolean f = false;
+		
+		
+		System.out.println(t);	
 		System.out.println("hello world2");
-
+		System.out.println(f);
 	}
-
 }
